2b8da8d8cfd41f624afaf6f371e6dc8ad3a7f025
YackobT <futur3look@gmail.com>
2025-04-23 18:39:06 -0400

    Fix crossOrigin attribute and finalize layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
     <html lang="en">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
           rel="stylesheet"
