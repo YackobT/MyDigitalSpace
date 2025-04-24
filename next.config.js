38ba7cd384ca276a4b801453c30a11ce7cf6f524
YackobT <futur3look@gmail.com>
2025-04-24 13:48:51 -0400

    Fix ReactNode import and cleanup config and imports

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  turbopack: {
-    enabled: true,
-  },
+  reactStrictMode: true,
+  swcMinify: true,
 };
 
 module.exports = nextConfig;
-
