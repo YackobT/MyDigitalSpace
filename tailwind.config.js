c51a5fc8b4f835309dce181d02d9e0469aaf13d9
Yackob <114114122+YackobT@users.noreply.github.com>
2025-04-25 17:22:21 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}", // Add this to scan src/components/
   ],
   theme: {
     extend: {},
