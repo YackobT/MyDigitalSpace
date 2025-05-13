1f9ca0d9bedf72aba719f65abc4877e2c8d234e9
YackobT <futur3look@gmail.com>
2025-05-13 16:41:53 -0400

    Fix missing imports and component references

--- src/app/projects/4/page.tsx
+++ src/app/projects/4/page.tsx
@@ -1,8 +1,8 @@
 'use client'
 
 import React from 'react'
-import Image from 'next/image'
 import { motion } from 'framer-motion'
+import Image from 'next/image'
 
 export default function Project4() {
   return (
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@
 
 import Link from 'next/link'
 import { useEffect, useState } from 'react'
-import { DarkModeToggle } from './DarkModeToggle'
+import { ThemeToggle } from './ThemeToggle'
 
 export default function Navbar() {
   const [scrolled, setScrolled] = useState(false)
