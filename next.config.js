68c155fad09e402d77fa81ec1d031b812e49bfca
Yackob <114114122+YackobT@users.noreply.github.com>
2025-06-12 20:48:35 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
+const isGithubPages = process.env.GITHUB_PAGES === 'true';
+
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
+  basePath: isGithubPages ? '/MyDigitalSpace' : '',
+  assetPrefix: isGithubPages ? '/MyDigitalSpace/' : '',
 };
 
 module.exports = nextConfig;
